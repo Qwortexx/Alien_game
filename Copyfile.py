8a935cb68751ad037c7fc9a7c6fdfcc52c59adb5
Qwortexx <romanstrashivskuy@gmail.com>
2025-04-16 20:02:43 +0300

    Update: add file copy

---
+++ Copyfile.py
@@ -0,0 +1,15 @@
+"Qwortex copy"
+'D:/Save/Ship6.bmp'
+"D:/Save/tiny_ship14.bmp"
+'D:/Save/background.bmp'
+'D:/Save/background_music.mp3'
+'D:/Save/short_shot_sound.wav'
+'D:/Save/hit_sound.wav'
+
+"D copy"
+'C:/Python/alien_gameShip6.bmp'
+"C:/Python/alien_gametiny_ship14.bmp"
+'C:/Python/alien_gamebackground.bmp'
+'C:/Python/alien_gamebackground_music.mp3'
+'C:/Python/alien_gameshort_shot_sound.wav'
+'C:/Python/alien_gamehit_sound.wav'
