352910fcca9bb44ae5984a071f952726b77f0191
Qwortexx <romanstrashivskuy@gmail.com>
2025-04-26 20:22:03 +0300

    Update: copyfile for Qwortex

--- Copyfile.py
+++ Copyfile.py
@@ -5,6 +5,7 @@
 'D:/Save/background_music.mp3'
 'D:/Save/short_shot_sound.wav'
 'D:/Save/hit_sound.wav'
+'C:/Users/PK/Downloads/sci-fi-braam-death-ray-om-fx-4-4-00-07.mp3'
 
 "Dany copy"
 'C:/Python/alien_game/Ship6.bmp'
